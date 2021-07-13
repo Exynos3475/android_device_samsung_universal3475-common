2ac9e9498d04ea8c9e7b220344e3fdcb216fc831
Seden Ecem <ttbpccorporation@outlook.com>
2021-07-13 15:49:56 +0300

    universal3475-common: Add TV Hal
    * For some reason, device bootloops without this package

--- device-common.mk
+++ device-common.mk
@@ -80,6 +80,10 @@ PRODUCT_HOST_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl
 
+# TV Input
+PRODUCT_PACKAGES += \
+    android.hardware.tv.input@1.0
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/configs/gps/gps.xml:system/etc/gps.xml
